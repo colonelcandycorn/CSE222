16a0e0cecc62c9598a80750445e64f506bec7f6c
Sarah Dylan <SarahmDylan@gmail.com>
2023-01-23 20:16:54 -0800

    finish testing main.c and fix makefile

--- PA2/main.c
+++ PA2/main.c
@@ -15,5 +15,32 @@ int main(int argc, char **argv) {
 	hashDist(myHash);
 	hashDump(myHash, hash("123")%50);
 	hashFree(myHash);
+
+	myHash = hashInit(750);
+	FILE *file = fopen("database.txt", "r");
+	char input[257];
+	char plate[257];
+	char first[257];
+	char last[257];
+	int error = 0;
+	if (!file) puts("ERROR");
+	else {
+		while (fgets(input, 257, file)) {
+			if (error = sscanf(input, "%s%s%s", plate, first, last) != 3) {
+				puts("ERROR inputting");
+				printf("Number scanned = %d\n", error);
+				printf("%s\n", input);
+			}
+			hashAdd(myHash, plate, first, last);
+		}
+	}
+	fclose(file);
+
+	for (int i = 0; i < 750; ++i) {
+		listPrint(myHash[i]);
+	}
+
+	hashDist(myHash);
+	hashFree(myHash);
 	return 0; 
 }
