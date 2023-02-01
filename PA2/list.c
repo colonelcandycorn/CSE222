5a895d468617194666d2d4f60473d60aa3d51170
Sarah Dylan <SarahmDylan@gmail.com>
2023-02-01 12:42:44 -0800

    do stuff

--- PA2/list.c
+++ PA2/list.c
@@ -1,10 +1,39 @@
+/*
+	Sarah Dylan CSE 215 PA2 01/30/2023
+
+	This is the implementation of a linked list. The implementation
+	is almost exactly the same as PA1 outside of the fact taht we our
+	nodes are also storing a first and last name. An another  difference
+	is we do not care the order in which the values are stored so we always
+	insert at the front of thelist. Because we don't know the size of the 
+	strings that will be needed to store the names and plate number, we will
+	have to dynamically allocate them. 
+
+*/
+
 #include "list.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+
+/* 
+	Parameters: none
+
+	initialize a list with a sentinel node. We store the string SENTINEL
+	in the plate field of the node and set the rest of the node. This would
+	be useful if needed to check if we were at sentinel, but for this particular
+	project it just matters that a node exists so we don't segfault with an empty
+	list.
+
+	Return: returns a pointer to the newly initialized sentinel node
+
+*/
+
+
 struct node *listInit() {
 	struct node *sent = (struct node *)malloc(sizeof(struct node));
-	char temp[] = "SENTINAL";
+	char temp[] = "SENTINEL";
 	char *sentStr = (char *)malloc(sizeof(char) * (strlen(temp) + 1));
 	strcpy(sentStr, temp);
 	sent->plate = sentStr;
@@ -13,6 +42,18 @@ struct node *listInit() {
 	sent->next = NULL;
 	return sent;
 }
+
+/*
+	Parameters: a sentinel node, a string for plate number, a string for the first name
+	and a string for the last name.
+
+	We then malloc memory for the node, then for each string based on the strlen + 1
+	because of the null value at the end of strings. We place this new node at the
+	front of the list so the time complexity is O(1).
+
+	Return: void
+
+*/
 
 void listAdd(struct node*sent, char *plate, char *firstname, char *lastname) {
 	struct node *temp = (struct node *)malloc(sizeof(struct node));
@@ -25,6 +66,17 @@ void listAdd(struct node*sent, char *plate, char *firstname, char *lastname) {
 	temp->next = sent->next;
 	sent->next = temp;
 }
+
+/*
+	Parameters: sentinel node, plate string, firstname, last name
+
+	The big difference here between add is we are using the first name and last name
+	parameters to store the found name so that value can be printed out later. We perform
+	our search starting at the sentinel and go until the end is reached so time complexity is
+	O(n)
+
+	Return: returns a 1 for success 0 for failure
+*/
 
 
 int listFind(struct node *sent, char *plate, char *firstname, char *lastname) {
@@ -42,6 +94,15 @@ int listFind(struct node *sent, char *plate, char *firstname, char *lastname) {
 	return 0;
 }
 
+/*
+	Parameter: sentinel node
+
+	We start at the sentinel node and keep moving until we reach null. Each
+	time we move we increment the counter.
+
+	Return: return the value of our counter as it represents the length of the linked list
+*/
+
 int listLen(struct node *sent) {
 	int temp = 0;
 	struct node *cur = sent->next;
@@ -52,6 +113,15 @@ int listLen(struct node *sent) {
 	return temp;
 }
 
+/*
+	Parameter: sentinel node
+	
+	Our PA2 project specifies that a format to print the value of the node. So we do the 
+	same as len but each node we visit we print the value. 
+
+	Return: void
+*/
+
 void listPrint(struct node *sent) {
 	struct node *cur = sent->next;
 	while (cur) {
@@ -59,6 +129,16 @@ void listPrint(struct node *sent) {
 	cur = cur->next;
 	}
 }
+
+/*
+	Parameter: sentinel
+
+	Similar to PA1 but different because we not only have to free the pointer to the structure
+	but the pointers for each string inside of the structure. So each node this time will free
+	4 times. 
+
+	Return: void
+*/
 
 void listFree(struct node *sent) {
 	struct node *cur = sent->next;
--- PA2/main.c
+++ PA2/main.c
@@ -1,18 +1,47 @@
+/*
+	Sarah Dylan CSE222 PA2 01/30/2023
+
+	This program is meant to demonstrate the functionality of my
+	implementation of a hash map. The program accepts 1 or 2 arguments
+	with the last argument always being the filename of a database. If
+	there are 2 arguments the first should be the size of hashmap the
+	user wants and then the last will be the file name. If no size is
+	provided, the program will default to a size of 50. 
+	
+	The type of file meant to be read is a list of license plates with
+	first and last names of the owners of the license plate. The hashmap
+	creates an associative array between the hash value of the license plate
+	and the associated first and last name. Because our hash function isn't perfect
+	and the size of the hashmap is dependent on user input collisions will be
+	unavoidable. Because of this our hashmap is an array of linked lists that will
+	allow more than one node to be stored at each position.
+
+	Our user can type in a plate and will receive back a first and last name.
+	Alternatively they can enter three commands: *DIST which prints the lengths
+	of each linked list at each array position, *DUMP someNumber will print all
+	the values in the linked list at someNumber, and finally *DUMP which will just
+	list every key-value pair out. 
+
+*/
+
 #include "hash.h"
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
 
-#define MAX_STR 257
+#define MAX_STR 257 // The documentaiton for PA2 stated 256 would be the limit for input
 
 int main(int argc, char **argv) {
-	char *dbName;
+	char *dbName; //store database file name stored by user
 	hashEntry* myHash;
-	int hashSize = 50;
+	int hashSize = 50; //default to fifty if user doesn't request a size
+	// argc equalling 2 means only one argument was supplied so initialize hash to default size
 	if (argc == 2 ) {
 		myHash = hashInit(50);
+	// otherwise use the size specified by user and store that size for later use
 	} else if (argc == 3 && (hashSize = atoi(argv[1])) >= 1) {
 		myHash = hashInit(hashSize);
+	// user must of have entered 0 or more than 2 arguments so ignore
 	} else {
 		puts("ERROR WITH ARGUMENTS");
 		return 1;
@@ -21,33 +50,43 @@ int main(int argc, char **argv) {
 	dbName = argv[argc - 1];
 
 	FILE *file = fopen(dbName, "r");
-	char input[MAX_STR];
+	char input[MAX_STR]; 
 	char plate[MAX_STR];
 	char first[MAX_STR];
 	char last[MAX_STR];
 	int scanNum = 0;
 	int cellNum = 0;
+
+	// make sure file opened or exists
 	if (!file) {
 		puts("ERROR READING FILE");
 		return 1;
+
+	//start scanning the file and store the plate, first, and last names into our hashmap
 	} else {
 		while (fgets(input, MAX_STR, file)) {
 			if (scanNum = sscanf(input, "%s%s%s", plate, first, last) != 3) {
-				puts("ERROR INPUTTING");
+				puts("ERROR INPUTTING"); //if the database is corrupted for some reason
 				return 1;
 			}
 			hashAdd(myHash, plate, first, last);
 		}
 	}
-	fclose(file);
+	fclose(file); 
 
-	
+	// our program has initialized correctly and we are ready for user input
+	// usre input will continue until EOF
 	char userInput[MAX_STR];
 	int isRunning = 1;
 	while (isRunning) {
 		printf("Enter command or plate: ");
 		if (!fgets(input, MAX_STR, stdin)) isRunning = 0; //check for EOF
 		scanNum = sscanf(input, "%s%d", userInput, &cellNum); //check for two args
+
+		//first we check if the user entered dumped and then we check if they entered
+		// a number. If they entered the number we have to make sure its a valid index
+		// otherwise, we will seg fault
+
 		if (!strcmp(userInput, "*DUMP")) {
 			if (scanNum == 2) {
 				if (cellNum >= hashSize || cellNum < 0) {
@@ -56,12 +95,21 @@ int main(int argc, char **argv) {
 				}
 				hashDump(myHash, cellNum);
 			} else {
+
+				// we need to loop through the entire hashmap
+				// use the size we stored earlier to loop through the array
 				for (int i = 0; i < hashSize; ++i) {
 					hashDump(myHash, i);
 				}
 			}
+
+		// call the has dist function
 		} else if (!strcmp(userInput, "*DIST")) {
 			hashDist(myHash);
+
+		// either they entered an incorrect command or they meant to look up a plate
+		// if the plate is incorrect or an incorrect command is typed it will say
+		// that a plate wasn't found
 		} else {
 			if (!hashFind(myHash, userInput, first, last)) {
 				puts("Plate not found.");
@@ -72,6 +120,8 @@ int main(int argc, char **argv) {
 		}
 	}
 	
+
+	// we reached EOF so free the memory and end
 	hashFree(myHash);
 	return 0; 
 }
