ff430f50795d519db2e2bbe516556570069d1cc1
Sarah Dylan <SarahmDylan@gmail.com>
2023-02-15 16:20:48 -0800

    Create tree.h file and finish it

---
+++ PA3/tree.h
@@ -0,0 +1,30 @@
+#ifndef PA3_TREE_H
+#define PA3_TREE_H
+
+struct node {
+	char *plate;
+	char *first;
+	char *last;
+	struct node *left;
+	struct node *right;
+};
+
+typedef struct node *Node;
+
+int height(Node root);
+Node add(Node root,
+		 char *plate,
+		 char *first,
+		 char *last);
+int search(Node root,
+		   char *plate,
+		   char *first,
+		   char *last);
+Node delete(Node root,
+		    char *plate);
+void LNR(Node root);
+void NLR(Node root);
+void LRN(Node root);
+
+void treeFree(Node root);
+#endif // PA3_TREE_H
