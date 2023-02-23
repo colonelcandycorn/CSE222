5c1f477d14ea9b3aa260063af7cd476030ab99e0
Sarah Dylan <SarahmDylan@gmail.com>
2023-02-23 15:05:51 -0800

    Finish commenting main

--- PA3/main.c
+++ PA3/main.c
@@ -1,20 +1,56 @@
+/*
+	Sarah Dylan CSE222 PA3 02/23/2023
+
+	This program is more or less the same as PA2: we store a database
+	of license plates that has an associated first and last name, and
+	allow the user to query the database and delete from it. The key
+	difference being that in this project the database is stored as a
+	BST. The BST offers O(log(n)) search times if the tree is balanced,
+	and it has the benefit of being sorted so we can print the license
+	plates in alphabetical order.
+
+	The program accepts exactly one argument which must be the name of the
+	database file. It returns an error if there are too few or too many args.
+	
+	We then initialize variables to store input, plates, first and last names,
+	and a scanNum (keeps track of how many things are entered by user). We open
+	the database file specfied by the user and make sure that it is read correctly.
+	We also initialize our tree to NULL. We read each line of the file, and each time
+	call the add function to add the new plate into our BST.
+
+	Once the file is read we close it and start taking commands. The user can enter
+	a plate to search for, *DUMP, or *DELETE 'plate'. If they just enter one argument
+	and it isn't the word '*DUMP' we assume they are searching for a plate and call the
+	search function. If the search function succeeds, we print out the name of the owner
+	of the specified plate. The *DUMP command follows the instructions provided by the
+	project instructions: we print the height, whether the tree is balanced, and print inorder
+	preorder and postorder. The *DELETE will first search for the plate and return an
+	error if it isn't there. If it is found, we delete the plate and print success.
+
+	The program will exit when it encounters EOF.
+
+*/
 #include <stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include "tree.h"
 
-#define MAX_STR 257
+#define MAX_STR 257 // arbitrary size for max length of string
+
 int main(int argc, char **argv) {
 	char *dbName;
 
+	// Confirm the correct argc value, which should be exactly 2
 	if (argc != 2) {
 		puts("Error starting program");
 		argc > 2 ? puts("too many arguments") : puts("too few arguments");
 		return 1;
 	}
 
+	// database file is stored in the second position of the arg array
 	dbName = argv[1];
 
+	//we don't have an init function so we set our tree to NULL
 	Node myTree = NULL;
 
 	FILE *file = fopen(dbName, "r");
@@ -23,14 +59,13 @@ int main(int argc, char **argv) {
 	char first[MAX_STR];
 	char last[MAX_STR];
 	int scanNum = 0;
-	int cellNum = 0;
 
 	// make sure file opened or exists
 	if (!file) {
 		puts("ERROR READING FILE");
 		return 1;
 
-	//start scanning the file and store the plate, first, and last names into our hashmap
+	//start scanning the file and store the plate, first, and last names into our tree
 	} else {
 		while (fgets(input, MAX_STR, file)) {
 			if (scanNum = sscanf(input, "%s%s%s", plate, first, last) != 3) {
@@ -42,17 +77,21 @@ int main(int argc, char **argv) {
 	}
 	fclose(file);
 
-	char userInput[MAX_STR];
-	char userPlate[MAX_STR];
-	int isRunning = 1;
+	// If we made it this far, we are open for business and can start accepting commands
+
+	char userInput[MAX_STR]; // used if one argument
+	char userPlate[MAX_STR]; // used if two arguments
+	int isRunning = 1; // we never change so could just use 1 instead of variable
 	while (isRunning) {
 		printf("Enter command or plate: "); 
 		if (!fgets(input, MAX_STR, stdin)) break; // exit if eof
 
 		scanNum = sscanf(input, "%s%s", userInput, userPlate); //check for two args
 
+
+		// if we have one argument and it's *DUMP then print everything we know
 		if (scanNum == 1 && !strcmp(userInput, "*DUMP")) {
-			//do stuff
+			//Print height, balance, inorder, preorder, and then postorder
 			printf("TREE HEIGHT: %d\n", height(myTree));
 			printf("BALANCED: %s\n\n", balanced(myTree) ? "YES" : "NO");
 			puts("LNR TRAVERSAL");
@@ -65,7 +104,7 @@ int main(int argc, char **argv) {
 			LRN(myTree);
 			puts("");
 		} else if (scanNum == 2 && !strcmp(userInput, "*DELETE")) {
-			//delete stuff
+			//Search and then delete
 			if (!search(myTree, userPlate, first, last)) {
 				 printf("user plate is %s\n", userPlate);
 				 puts("PLATE NOT FOUND");
@@ -75,7 +114,7 @@ int main(int argc, char **argv) {
 			myTree = delete(myTree, userPlate);
 			puts("SUCCESS");
 		} else {
-			// search stuff
+			// One argument and not *DUMP means we just search for plate
 			if (!search(myTree, userInput, first, last)) {
 				puts("PLATE NOT FOUND");
 				continue;
@@ -85,6 +124,6 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	treeFree(myTree);
+	treeFree(myTree); // Free all of our memory
 	return 0;
 }
